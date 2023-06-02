9c91ca490176445e755b7dae8935075e95735da2
kevin-donga <dev.kevindonga@gmail.com>
2023-06-02 14:32:41 +0530

    last

--- lib/screen/bottom_navbar_screen/bottom_navbar_screen_widget.dart
+++ lib/screen/bottom_navbar_screen/bottom_navbar_screen_widget.dart
@@ -4,7 +4,6 @@ import 'package:get/get.dart';
 import 'package:poetry/common/iconbutton_widget.dart';
 import 'package:poetry/common/text_widget.dart';
 import 'package:poetry/utils/asset_res.dart';
-import 'package:poetry/utils/asset_res.dart';
 
 import '../../utils/color_res.dart';
 import '../../utils/string_res.dart';
@@ -27,136 +26,133 @@ List<Widget> screen = [
 Widget bottomNavBar() {
   double h = Get.height;
   double w = Get.width;
-  return GetBuilder<BottomNavBarController>(builder: (controller) {
-    return Stack(
-      children: [
-        SizedBox(
-          height: h,
-          width: w,
-          child: screen[controller.index],
-        ),
-        Positioned(
-          bottom: 0,
-          left: 0,
-          child: SizedBox(
-            width: w,
-            height: h * 0.080,
-            child: Stack(
-              children: [
-                CustomPaint(
-                  size: Size(w, 80),
-                  painter: CustomPainterWidget(),
-                ),
-                Center(
-                  heightFactor: 0.6,
-                  child: GetBuilder<BottomNavBarController>(
-                      id: "addPoemIcon",
-                      builder: (controller) {
-                        return FloatingActionButton(
-                          onPressed: controller.addpoemIcon,
-                          backgroundColor: Colors.white,
-                          child: Padding(
-                            padding: const EdgeInsets.all(5.0),
-                            child: DottedBorder(
-                              color: ColorRes.greenColor,
-                              borderType: BorderType.Circle,
-                              child: Padding(
-                                padding: const EdgeInsets.all(8.0),
-                                child: Image.asset(
-                                  color: controller.index == 2
-                                      ? Colors.white
-                                      : ColorRes.greenColor,
-                                  AssetRes.bottomNavBarCenterFeatherImage,
-                                  fit: BoxFit.fill,
-                                ),
+  return GetBuilder<BottomNavBarController>(
+      id: "navigationBar",
+      builder: (controller) {
+        return Stack(
+          children: [
+            SizedBox(
+              height: h,
+              width: w,
+              child: screen[controller.selectedIndex],
+            ),
+            Positioned(
+              bottom: 0,
+              left: 0,
+              child: SizedBox(
+                width: w,
+                height: h * 0.080,
+                child: Stack(
+                  children: [
+                    CustomPaint(
+                      size: Size(w, 80),
+                      painter: CustomPainterWidget(),
+                    ),
+                    Center(
+                      heightFactor: 0.6,
+                      child: FloatingActionButton(
+                        onPressed: () => controller.onTapBottom(2),
+                        backgroundColor: Colors.white,
+                        child: Padding(
+                          padding: const EdgeInsets.all(5.0),
+                          child: DottedBorder(
+                            color: ColorRes.greenColor,
+                            borderType: BorderType.Circle,
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Image.asset(
+                                color: controller.selectedIndex == 2
+                                    ? Colors.white
+                                    : ColorRes.greenColor,
+                                AssetRes.featherImage,
+                                fit: BoxFit.fill,
                               ),
                             ),
                           ),
-                        );
-                      }),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      height: h * 0.70,
+                      width: w,
+                      decoration: const BoxDecoration(
+                          borderRadius: BorderRadius.only(
+                              topRight: Radius.circular(20),
+                              topLeft: Radius.circular(20))),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Column(
+                            children: [
+                              commonIconButton(
+                                  onPressed: () => controller.onTapBottom(0),
+                                  icon: Icon(controller.selectedIndex == 0
+                                      ? Icons.home
+                                      : Icons.home_outlined)),
+                              commonTextWidget(text: StringRes.homeText)
+                            ],
+                          ),
+                          Column(
+                            children: [
+                              GetBuilder<BottomNavBarController>(
+                                  id: 'navigationBar',
+                                  builder: (controller) {
+                                    return commonIconButton(
+                                        onPressed: () =>
+                                            controller.onTapBottom(1),
+                                        icon: Icon(controller.selectedIndex == 1
+                                            ? Icons.bookmark
+                                            : Icons.bookmark_border));
+                                  }),
+                              commonTextWidget(text: StringRes.bookmarkText)
+                            ],
+                          ),
+                          Container(
+                            width: w * 0.15,
+                          ),
+                          Column(
+                            children: [
+                              GetBuilder<BottomNavBarController>(
+                                  id: 'navigationBar',
+                                  builder: (controller) {
+                                    return commonIconButton(
+                                        onPressed: () =>
+                                            controller.onTapBottom(3),
+                                        icon: Icon(controller.selectedIndex == 3
+                                            ? Icons.notifications
+                                            : Icons.notifications_none));
+                                  }),
+                              commonTextWidget(text: StringRes.notificationText)
+                            ],
+                          ),
+                          Column(
+                            children: [
+                              GetBuilder<BottomNavBarController>(
+                                  id: 'bookmarkIcon',
+                                  builder: (controller) {
+                                    return commonIconButton(
+                                        onPressed: () =>
+                                            controller.onTapBottom(4),
+                                        icon: Icon(controller.selectedIndex == 4
+                                            ? Icons.person
+                                            : Icons.person_outline));
+                                  }),
+                              commonTextWidget(text: StringRes.profileText)
+                            ],
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
                 ),
-                Container(
-                  height: h * 0.70,
-                  width: w,
-                  decoration: const BoxDecoration(
-                      borderRadius: BorderRadius.only(
-                          topRight: Radius.circular(20),
-                          topLeft: Radius.circular(20))),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      Column(
-                        children: [
-                          GetBuilder<BottomNavBarController>(
-                              id: 'homeScreenIcon',
-                              builder: (controller) {
-                                return commonIconButton(
-                                    onPressed: controller.homeScreenIcon,
-                                    icon: Icon(controller.index == 0
-                                        ? Icons.home
-                                        : Icons.home_outlined));
-                              }),
-                          commonTextWidget(text: StringRes.homeText)
-                        ],
-                      ),
-                      Column(
-                        children: [
-                          GetBuilder<BottomNavBarController>(
-                              id: 'bookmarkIcon',
-                              builder: (controller) {
-                                return commonIconButton(
-                                    onPressed: controller.bookmarkIcon,
-                                    icon: Icon(controller.index == 1
-                                        ? Icons.bookmark
-                                        : Icons.bookmark_border));
-                              }),
-                          commonTextWidget(text: StringRes.bookmarkText)
-                        ],
-                      ),
-                      Container(
-                        width: w * 0.15,
-                      ),
-                      Column(
-                        children: [
-                          GetBuilder<BottomNavBarController>(
-                              id: 'notificationIcon',
-                              builder: (controller) {
-                                return commonIconButton(
-                                    onPressed: controller.notificationIcon,
-                                    icon: Icon(controller.index == 3
-                                        ? Icons.notifications
-                                        : Icons.notifications_none));
-                              }),
-                          commonTextWidget(text: StringRes.notificationText)
-                        ],
-                      ),
-                      Column(
-                        children: [
-                          GetBuilder<BottomNavBarController>(
-                              id: 'bookmarkIcon',
-                              builder: (controller) {
-                                return commonIconButton(
-                                    onPressed: controller.profileIcon,
-                                    icon: Icon(controller.index == 4
-                                        ? Icons.person
-                                        : Icons.person_outline));
-                              }),
-                          commonTextWidget(text: StringRes.profileText)
-                        ],
-                      ),
-                    ],
-                  ),
-                )
-              ],
-            ),
-          ),
-        )
-      ],
-    );
-  });
+              ),
+            )
+          ],
+        );
+      });
 }
 
-class custompainter extends CustomPainter {
+class CustomPainterWidget extends CustomPainter {
   @override
   void paint(Canvas canvas, Size size) {
     Paint paint = Paint()
